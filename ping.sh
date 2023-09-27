a44302bb86b6cf1c8f139e33d15ea4c3fbc32895
Hossein3piol <mhsaeidi81@gmail.com>
2023-09-27 04:47:56 +0330

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,7 @@
+##########      install speedtest      ############
+sudo apt-get install curl -y
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+sudo apt-get install speedtest
+clear;
+
+##########      End install speedtest      ############
